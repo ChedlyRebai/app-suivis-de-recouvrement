e922b762428b2629baa3bdb36bca93a2f533e556
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2024-02-26 23:57:59 +0200

    addDropdown

--- app/(root)/access-management/page.tsx
+++ app/(root)/access-management/page.tsx
@@ -1,10 +1,48 @@
-const Page = () => {
+import { Payment, columns } from "./_component/columns";
+import { DataTable } from "./_component/data-table";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "c@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <div>
-      sssasasa
-      <p>ssssss</p>
+      <DataTable columns={columns} data={data} />
     </div>
   );
-};
-
-export default Page;
+}
