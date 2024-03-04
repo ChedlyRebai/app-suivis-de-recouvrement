84b0bbbb3761afe5a8b5a615b8307e7a3bf30035
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2024-03-04 15:46:03 +0200

    add navbar actions

---
+++ actions/navbar.action.ts
@@ -0,0 +1,19 @@
+import { getSession } from "@/lib";
+import axios from "axios";
+
+export const getLinksByCodeFonction = async (matricule: string) => {
+  try {
+    const user = await getSession();
+    console.log(user);
+    console.log(`${process.env.API_URL}/user`);
+
+    axios.defaults.baseURL = "http://localhost:10000";
+    const response = await axios.get(
+      `/getLinks?codeFunction=${user.code_function}`
+    );
+    console.log(response.data);
+    return response.data || [];
+  } catch (error) {
+    return "";
+  }
+};
