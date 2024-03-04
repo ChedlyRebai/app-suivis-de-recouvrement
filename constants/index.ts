78f6cdb521757e8e3f518264f2f67b23a31ab94f
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2024-03-04 15:37:42 +0200

    up iconMap

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,6 @@
-import { droit_accees } from "@/Models/droit_accees.model";
 import { HomeIcon, UsersIcon } from "lucide-react";
-import { IconType } from "react-icons";
 
-const iconMap = {
+export const iconMap = {
   "consicons/ug": HomeIcon,
   "consicons/users": UsersIcon,
 };
