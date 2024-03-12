ca1fc01f351c1dc5a82810a50222bcedff442c6f
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2024-03-12 16:25:35 +0200

    update column

--- app/[locale]/(root)/access-management/_component/columns.tsx
+++ app/[locale]/(root)/access-management/_component/columns.tsx
@@ -144,34 +144,8 @@ export const columns: ColumnDef<droit_accees>[] = [
       );
     },
   },
-  // {
-  //   accessorKey: "email",
-  // header: ({ column }) => {
-  //   return (
-  //     <Button
-  //       variant="ghost"
-  //       onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-  //     >
-  //       Email
-  //       <ArrowUpDown className="ml-2 h-4 w-4" />
-  //     </Button>
-  //   );
-  // },
-  // },
-  // {
-  //   accessorKey: "amount",
-  //   header: () => <div className="text-right">Amount</div>,
-  //   cell: ({ row }) => {
-  //     const amount = parseFloat(row.getValue("amount"));
-  //     const formatted = new Intl.NumberFormat("en-US", {
-  //       style: "currency",
-  //       currency: "USD",
-  //     }).format(amount);
 
-  //     return <div className="text-right font-medium">{formatted}</div>;
-  //   },
-  // },
-  {
+  /* {
     id: "actions",
     cell: ({ row }) => {
       const payment = row.original;
@@ -213,10 +187,10 @@ export const columns: ColumnDef<droit_accees>[] = [
 
             {/* <DropdownMenuSeparator />
             <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem> */}
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       );
     },
-  },
+  },*/
 ];
