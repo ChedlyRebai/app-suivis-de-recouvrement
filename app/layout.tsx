353f280dec4775a2e68367f594e5af51d669544a
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2024-03-15 03:58:38 +0200

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 import axios from "axios";
 import ToastProviders from "@/providers/ToastProviders";
 import ModalProviders from "@/providers/ModalProviders";
