e1cc3172494fa3e5e453dd8db6d80eaa3242e523
ChedlyRebai <chedly.rebai123@gmail.com>
2024-03-23 17:01:42 +0100

    up

--- components/forms/Compte-rendu-form/PromiseDereglement.tsx
+++ components/forms/Compte-rendu-form/PromiseDereglement.tsx
@@ -8,6 +8,7 @@ import { CalendarIcon } from 'lucide-react'
 import React, { useState } from 'react'
 
 import { format } from "date-fns";
+import { DatePickerDemo } from '@/components/ui/DatePicker'
 const PromiseDereglement = () => {
     const [date, setdate] = useState(new Date())
   return (
@@ -34,27 +35,7 @@ const PromiseDereglement = () => {
                     >
                       Date
                     </Label>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={"outline"}
-                          className={cn(
-                            "w-[280px] justify-start text-left font-normal",
-                            !date && "text-muted-foreground"
-                          )}
-                        >
-                          <CalendarIcon className="mr-2 h-4 w-4" />
-                          {date ? (
-                            format(date, "PPP")
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0">
-                        <Calendar mode="single" selected={date} initialFocus />
-                      </PopoverContent>
-                    </Popover>
+                    <DatePickerDemo />
                   </div>
                   <div className="flex flex-col">
                     <Label
