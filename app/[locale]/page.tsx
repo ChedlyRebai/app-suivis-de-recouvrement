bcc0f9508c662ba31a026cf8897e09918409af4d
ChedlyRebai <chedly.rebai123@gmail.com>
2024-03-29 03:06:42 +0100

    up

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,19 +5,10 @@ import Image from "next/image";
 import Navbar from "./(root)/access-management/_component/MainLayout";
 import Mainlayout from "./(root)/access-management/_component/MainLayout";
 
-export async function Page ({
-  children,
-}:{
-  children: any;
-})  {
-  const links = await getLinksByCodeFonction();
-  const t = await getTranslations("access-management");
-  const session = await getSession();
+export async function Page ()  {
 
   return (
-    <Mainlayout showSidebar links={links} title={t("title")} session={session}>
-      {children}
-    </Mainlayout>
+    <div></div>
   );
 };
 
