66bd77d9f9eebd55390b984084d2a501f35b6690
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-06 17:19:04 +0100

    up

---
+++ hooks/use-compte-rendu-modal.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+interface AddDroitModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useAddDroitModal = create<AddDroitModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useAddDroitModal;
