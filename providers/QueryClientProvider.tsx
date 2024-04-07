59fad443dc34643f43a372c9b6b7c9d27d5b7622
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-07 03:04:30 +0100

    add ReactQueryProvider

---
+++ providers/QueryClientProvider.tsx
@@ -0,0 +1,18 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+  } from '@tanstack/react-query'
+import { ReactNode } from 'react'
+  
+const queryClient = new QueryClient()
+
+const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      )
+}
+
+export default ReactQueryProvider
