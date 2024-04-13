730d31c3802f6bcad788e5484697a675f2e21714
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-13 19:40:34 +0100

    Update next.config.mjs with output and experimental options

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // (Optional) Export as a standalone site
+    // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
+    output: 'standalone', // Feel free to modify/remove this option
+    
+    // Indicate that these packages should not be bundled by webpack
+    experimental: {
+        serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+    },
+};
 
 export default withNextIntl(nextConfig);
+
