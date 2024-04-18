126e1ba85d45a628d6bd9486bad0623f189e6310
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-18 08:49:29 +0100

    Update loading.tsx to add className for styling

--- app/[locale]/(root)/ListeClient/loading.tsx
+++ app/[locale]/(root)/ListeClient/loading.tsx
@@ -4,7 +4,7 @@ import { Oval } from "react-loading-icons";
 const laoding = () => {
   return (
     <div className="absolute top-[50%] right-[50%] flex items-center justify-center">
-      <Oval />
+      <Oval className="text-gray-800 dark:text-gray-200" />
     </div>
   );
 };
