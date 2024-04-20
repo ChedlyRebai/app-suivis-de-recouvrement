34286385d736805968d727eef26d8d03e558f250
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-20 12:28:24 +0100

    update

--- app/[locale]/(root)/validation-proposition-de-proposition-anticipé/page.tsx
+++ app/[locale]/(root)/validation-proposition-de-proposition-anticipé/page.tsx
@@ -1,8 +1,94 @@
+"use server";
 
-const page = () => {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  getAgences,
+  getClientContactes,
+  getClientNonContactes,
+  getGroupes,
+} from "@/actions/client.action";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Suspense } from "react";
+
+import { getLettre } from "@/actions/lettre.action";
+import { DataTableDemandeDeTransfer } from "./_component/demande-transfer-data-table";
+import { demandeTransferColumns } from "./_component/demandeTransferColumns";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+    limit?: string;
+    perPage?: string;
+    groupe?: string;
+    agence?: string;
+    to?: string;
+    from?: string;
+  };
+}) {
+  const search = searchParams?.query || "";
+  const group = searchParams?.groupe || "";
+  const agence = searchParams?.agence || "";
+  const from = searchParams?.from || "";
+  const to = searchParams?.to || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const perPage = Number(searchParams?.perPage) || 5;
+  const limit = Number(searchParams?.limit) || 20;
+
+  const data = await getLettre(
+    search,
+    currentPage,
+    perPage,
+    group,
+    agence,
+    from,
+    to
+  );
+  console.log("data");
+  console.log(data);
+
+  const groupes = await getGroupes();
+  const agences = await getAgences();
+  console.log("render page");
   return (
-    <div>page</div>
-  )
+    <div className="bg-muted/40 min-h-screen">
+      <div className="py-6 mt-16">
+        <div className=" mx-auto px-4 sm:px-6 md:px-8">
+          <h1 className="text-2xl font-semibold text-gray-900 dark:text-white"></h1>
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className=" mx-auto px-4 sm:px-6 md:px-8">
+            <Card>
+              <CardHeader>
+                <CardTitle>Demande de transfer anticipé</CardTitle>
+                {/* <CardDescription>
+                      Manage your products and view their sales performance.
+                    </CardDescription> */}
+              </CardHeader>
+              <CardContent>
+                <DataTableDemandeDeTransfer
+                  agences={agences || []}
+                  groupes={groupes || []}
+                  total={data.total || 0}
+                  totalAccout={data.totalCount || 0}
+                  totalPages={data.totalPages || 1}
+                  columns={demandeTransferColumns}
+                  data={data.result || []}
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </Suspense>
+      </div>
+    </div>
+  );
 }
-
-export default page
