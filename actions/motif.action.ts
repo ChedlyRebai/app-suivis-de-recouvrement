49d2890e09ce9be1942b5caf695358ef560a3013
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-25 01:30:03 +0100

    Update motif.action.ts to use ab_cxrepenv model

--- actions/motif.action.ts
+++ actions/motif.action.ts
@@ -1,30 +1,31 @@
 "use server ";
 
 import { Cxuntges } from "@/Models/Cxuntges.model";
+import { ab_cxrepenv } from "@/Models/ab_cxrepenv.model";
 import axios from "axios";
 
 export const getMotif = async () => {
   try {
     axios.defaults.baseURL = `${process.env.API_URL}`;
-    const response = await axios.get<Cxuntges[]>(
+    const response = await axios.get<ab_cxrepenv[]>(
       `http://localhost:10001/client/motif`
     );
-    return response.data || [] as Cxuntges[];
+    return response.data || [] as ab_cxrepenv[];
   } catch (error) {
     console.log(error);
-    return [] as Cxuntges[];
+    return [] as ab_cxrepenv[];
   }
 };
 
 export const getMotifCommercial = async () => {
   try {
     axios.defaults.baseURL = `${process.env.API_URL}`;
-    const response = await axios.get<Cxuntges[]>(
+    const response = await axios.get<ab_cxrepenv[]>(
       `http://localhost:10001/client/motifcommercial`
     );
-    return response.data || [] as Cxuntges[];
+    return response.data || [] as ab_cxrepenv[];
   } catch (error) {
     console.log(error);
-    return [] as Cxuntges[];
+    return [] as ab_cxrepenv[];
   }
 }
