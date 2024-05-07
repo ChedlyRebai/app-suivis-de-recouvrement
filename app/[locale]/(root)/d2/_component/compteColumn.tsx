3a823f011bda119cfa8187c925695368fd6038bb
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-07 17:01:37 +0100

    refactor: Add compteColumn component for displaying compte data in a table

---
+++ app/[locale]/(root)/d2/_component/compteColumn.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { ab_client } from "@/Models/ab_client.model";
+import { Client, Compte, Utilisateur, clientResult } from "@/actions/admin.action";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+export const comptecolumns: ColumnDef<Compte>[] = [
+  {
+    accessorKey: "cli",
+    header: "cli",
+  },
+  {
+    accessorKey: "ab_client.nom",
+    header: "Nom de client",
+  },
+  {
+    accessorKey: "Agence",
+    header: "Agence",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {`${row.original.Agence.codug} : ${row.original.Agence.libelle}`}{" "}
+        </span>
+      );
+    },
+  },
+
+  {
+    accessorKey: "mnt_imp",
+    header: "Montant impayé",
+  },
+
+  {
+    accessorKey: "mnt_imp",
+    header: "Montant solde debiteur", 
+  },
+
+
+ 
+];
