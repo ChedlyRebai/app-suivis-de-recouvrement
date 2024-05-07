91dea979c3ff65ac1b0cb72b66ead142fdd1826a
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-07 23:59:04 +0100

    refactor: Add powerbi-client-react dependency to package.json

---
+++ app/[locale]/(root)/d2/analytics/c.tsx
@@ -0,0 +1,45 @@
+import { PowerBIEmbed } from 'powerbi-client-react';
+import React from 'react'
+import { models } from 'powerbi-client';
+const c = () => {
+  return (
+    <>
+    <PowerBIEmbed
+	embedConfig = {{
+		type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+		id: '<Report Id>',
+		embedUrl: '<Embed Url>',
+		accessToken: '<Access Token>',
+		tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+		settings: {
+			panes: {
+				filters: {
+					expanded: false,
+					visible: false
+				}
+			},
+			background: models.BackgroundType.Transparent,
+		}
+	}}
+
+	eventHandlers = {
+		new Map([
+			['loaded', function () {console.log('Report loaded');}],
+			['rendered', function () {console.log('Report rendered');}],
+			['error', function (event:any) {console.log(event.detail);}],
+			['visualClicked', () => console.log('visual clicked')],
+			['pageChanged', (event) => console.log(event)],
+		])
+	}
+
+	cssClassName = { "reportClass" }
+
+	getEmbeddedComponent = { (embeddedReport:any) => {
+		window.Report = embeddedReport ;
+	}}
+/>
+    </>
+  )
+}
+
+export default c
---
+++ app/[locale]/(root)/d2/analytics/page.tsx
@@ -0,0 +1,21 @@
+import {
+  getAllAccount,
+  getAllClient,
+  getAllCompteRendu,
+  getAllUsers,
+} from "@/actions/admin.action";
+import React from "react";
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { AllUsers } from "../_component/alllusers";
+import { Utilisateurcolumns } from "../_component/Utilisateurcolumns";
+const page =()=>{
+  
+  return (
+    
+   <>
+   
+   </>
+  );
+}
+
+export default page;
