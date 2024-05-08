ea396a9e13a711b90762c5a73b2602ab66a09227
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-08 23:51:15 +0100

    refactor: Add drag and drop functionality to UploadFiles component

--- components/shared/Modals/Upload-file-Modal.tsx
+++ components/shared/Modals/Upload-file-Modal.tsx
@@ -1,20 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
 import Modal from "./Modal";
 import useAddDroitModal from "@/hooks/useAddDroitModal";
 import AddDroitForm from "@/components/forms/AddDroitForm";
+import { useDropzone } from "react-dropzone";
 
 const UploadFiles = () => {
-  const { isOpen, onOpen, onClose } = useAddDroitModal();
+    const onDrop = useCallback((acceptedFiles:any) => {
+        // Do something with the files
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    
+
   return (
     <Modal
       title="Ajouter un droits d'accès"
       description="Trouvez et mettez à jour vos fonctions en quelques clics"
-      isOpen={isOpen}
-      onChange={onClose}
+      isOpen={true}
+      onChange={() => {}}
     >
-      Uplaoud
+      <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
     </Modal>
   );
 };
