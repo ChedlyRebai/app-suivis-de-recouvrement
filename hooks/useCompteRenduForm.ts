bf34e2034c46e3eef3acdb49ce170b09f93212ba
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-19 19:30:46 +0100

    refactor: Update useCompteRenduForm.ts to improve code readability and type annotations

--- hooks/useCompteRenduForm.ts
+++ hooks/useCompteRenduForm.ts
@@ -4,34 +4,44 @@ import { createCompteRendu } from "@/actions/client.action";
 import { create } from "zustand";
 
 interface useClientSoreInterface {
-    client: ClientRecouv ;
-    suiviAgenda:SuiviAgenda;
-    compteRendu:any;
-    seTsuiAgenda:(suiviAgenda:SuiviAgenda)=>void;
-    setClient: (client: ClientRecouv) => void;
-    handleIputChangeSuiviAgenda: (champ:string,value:string) => void;
-    saveSuiviAgenda: (suiviAgenda:SuiviAgenda,compteRendu:any,cli:string,type:string) =>Promise<void>;
+  client: any;
+  suiviAgenda: SuiviAgenda;
+  compteRendu: any;
+  seTsuiAgenda: (suiviAgenda: SuiviAgenda) => void;
+  setClient: (client: any) => void;
+  handleIputChangeSuiviAgenda: (champ: string, value: string) => void;
+  saveSuiviAgenda: (
+    suiviAgenda: SuiviAgenda,
+    compteRendu: any,
+    cli: string,
+    type: string
+  ) => Promise<void>;
 }
 
 const useClientSore = create<useClientSoreInterface>((set) => ({
-    client: {} as ClientRecouv,
-    suiviAgenda:{} as SuiviAgenda,
-    seTsuiAgenda:(suiviAgenda:SuiviAgenda)=>set({suiviAgenda}),
-    setClient: (client) => set({ client }),
-    compteRendu:"",
-    handleIputChangeSuiviAgenda: (champ,value) => {
-        set((state) => ({
-            suiviAgenda: {
-                ...state.suiviAgenda,
-                [champ]: value,
-            },
-        }));
-    },
+  client: {} as any,
+  suiviAgenda: {} as SuiviAgenda,
+  seTsuiAgenda: (suiviAgenda: SuiviAgenda) => set({ suiviAgenda }),
+  setClient: (client) => set({ client }),
+  compteRendu: "",
+  handleIputChangeSuiviAgenda: (champ, value) => {
+    set((state) => ({
+      suiviAgenda: {
+        ...state.suiviAgenda,
+        [champ]: value,
+      },
+    }));
+  },
 
-    saveSuiviAgenda: async (suiviAgenda,compteRendu,cli,type) => {
-        const savedSuiviAgenda=await createCompteRendu(suiviAgenda,compteRendu,cli,type);
-        return savedSuiviAgenda;    
-    },
+  saveSuiviAgenda: async (suiviAgenda, compteRendu, cli, type) => {
+    const savedSuiviAgenda = await createCompteRendu(
+      suiviAgenda,
+      compteRendu,
+      cli,
+      type
+    );
+    return savedSuiviAgenda;
+  },
 }));
 
 export default useClientSore;
