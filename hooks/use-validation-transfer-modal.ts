5889955ee453989aa1751ad3198db05e5a81eb34
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-25 00:26:21 +0100

    refactor: Update use-validation-transfer-modal.ts
    - Add Motif and commentaire properties to ValidationTrnasferModalStore interface
    - Add setMotifCommentaire and setId functions to ValidationTrnasferModalStore
    - Initialize commentaire and Motif as undefined in useValidationTransferModal
    - Update setId function to set the id in the store
    - Update setMotifCommentaire function to set the Motif and commentaire in the store
    - Remove unused id property from ValidationTrnasferModalStore interface

--- hooks/use-validation-transfer-modal.ts
+++ hooks/use-validation-transfer-modal.ts
@@ -1,17 +1,24 @@
 import { create } from "zustand";
 
 interface ValidationTrnasferModalStore {
-  id?: string | number;
   isOpen: boolean;
+  id: string | undefined;
+  Motif: string | undefined;
+  commentaire: string | undefined;
+  setMotifCommentaire: (Motif: string, commentaire: string) => void;
+  setId: (id: string) => void;
   onOpen: () => void;
-  setId: (id: string | number) => void;
   onClose: () => void;
 }
 
 const useValidationTransferModal = create<ValidationTrnasferModalStore>(
   (set) => ({
+    commentaire: undefined,
+    Motif: undefined,
     id: undefined,
-    setId: (id: string | number) => set({ id }),
+    setMotifCommentaire: (Motif = "", commentaire = "") =>
+      set({ Motif, commentaire }),
+    setId: (id) => set({ id }),
     isOpen: false,
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false }),
