21c3be92c1d357c153d387983c966589e02baef8
ChedlyRebai <chedly.rebai123@gmail.com>
2024-06-19 15:09:32 +0100

    remove meta access from allHistorique component

--- app/[locale]/(root)/d2/files/_components/allHistoriques.tsx
+++ app/[locale]/(root)/d2/files/_components/allHistoriques.tsx
@@ -133,9 +133,6 @@ export function AllHistoriques({
     getSortedRowModel: getSortedRowModel(),
     getFacetedRowModel: getFacetedRowModel(),
     getFacetedUniqueValues: getFacetedUniqueValues(),
-    meta: {
-      access,
-    },
   });
 
   const [loader, setLoader] = useState(true);
