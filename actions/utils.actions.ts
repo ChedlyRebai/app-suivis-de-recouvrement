5fd96d8cf1fd836da55072f555f2fd39dde28c1a
ChedlyRebai <chedly.rebai123@gmail.com>
2024-06-19 15:07:32 +0100

    up

--- actions/utils.actions.ts
+++ actions/utils.actions.ts
@@ -1,5 +1,5 @@
 "use server ";
-import * as pdfjsLib from "pdfjs-dist";
+
 import { Cxuntges } from "@/Models/Cxuntges.model";
 import axios from "axios";
 
@@ -8,47 +8,47 @@ interface Main {
   libelle: string;
 }
 
-export default async function extractTextFromPDF(file: File) {
-  const loadingTask = pdfjsLib.getDocument(URL.createObjectURL(file));
-  const pdf = await loadingTask.promise;
-  let extractedText = "";
+// export default async function extractTextFromPDF(file: File) {
+//   const loadingTask = pdfjsLib.getDocument(URL.createObjectURL(file));
+//   const pdf = await loadingTask.promise;
+//   let extractedText = "";
 
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i);
-    const textContent = await page.getTextContent();
-    const pageText = textContent.items.map((item: any) => item.str).join(" ");
-    extractedText += pageText + " ";
-  }
+//   for (let i = 1; i <= pdf.numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const textContent = await page.getTextContent();
+//     const pageText = textContent.items.map((item: any) => item.str).join(" ");
+//     extractedText += pageText + " ";
+//   }
 
-  return extractedText.trim();
-}
+//   return extractedText.trim();
+// }
 
-export const transdata = async (data: ArrayBuffer): Promise<any> => {
-  try {
-    const typedArray = new Uint8Array(data);
+// export const transdata = async (data: ArrayBuffer): Promise<any> => {
+//   try {
+//     const typedArray = new Uint8Array(data);
 
-    const pdf = await pdfjsLib.getDocument({ data: typedArray }).promise;
-    console.log("PDF loaded");
+//     const pdf = await pdfjsLib.getDocument({ data: typedArray }).promise;
+//     console.log("PDF loaded");
 
-    const page = await pdf.getPage(1);
-    console.log("Page loaded");
+//     const page = await pdf.getPage(1);
+//     console.log("Page loaded");
 
-    const textContent = await page.getTextContent();
-    let text = "";
+//     const textContent = await page.getTextContent();
+//     let text = "";
 
-    textContent.items.forEach((item: any) => {
-      text += item.str + " ";
-    });
+//     textContent.items.forEach((item: any) => {
+//       text += item.str + " ";
+//     });
 
-    // Display text content
-    console.log(text);
+//     // Display text content
+//     console.log(text);
 
-    return [];
-  } catch (error) {
-    console.error(error);
-    return [] as Main[];
-  }
-};
+//     return [];
+//   } catch (error) {
+//     console.error(error);
+//     return [] as Main[];
+//   }
+// };
 
 // export const extractTextFromPDF = async (file: File): Promise<string> => {
 //   const loadingTask = pdfjsLib.getDocument(URL.createObjectURL(file));
