c30f42217b6b3e211cb64a3c203f06fd7cd1b279
ChedlyRebai <chedly.rebai123@gmail.com>
2024-06-19 15:44:59 +0100

    up

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -48,13 +48,13 @@ const Navbar = ({ onChange, session }: NavbarProps) => {
   };
   return (
     <div className="fixed w-screen top-0 z-10 flex-shrink-0 flex h-20  backdrop-blur-3xl shadow dark:border-b ">
-      <button
+      {/* <button
         type="button"
         className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
       >
         <span className="sr-only">Open sidebar</span>
         <MenuIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
+      </button> */}
       {pathname !== `/${local}` && (
         <button
           type="button"
